d0b295afcb410cf855a4d77a53daa3ffce93fdb3
khushi <khushitanwar0005@gmail.com>
2026-01-28 15:47:36 +0530

    session 6 js

---
+++ JS-DAY-6/script.js
@@ -0,0 +1,10 @@
+// document object model
+
+
+// const headingElement = document.
+// getElementById("myHeading");
+// console.log(headingElement);
+
+// headingElement.textContent = "New Heading Text";
+// headingElement.style.color = "blue";
+// headingElement.style.backgroundColor = "yellow";
